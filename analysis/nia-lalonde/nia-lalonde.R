ef6ec6c1d68a906bfe7279ae68f3776e1f17523e
Andriy V. Koval <koval.andrey@gmail.com>
2021-12-20 09:46:51 -0700

    for CBM session

--- analysis/nia-lalonde/nia-lalonde.R
+++ analysis/nia-lalonde/nia-lalonde.R
@@ -11,7 +11,7 @@ cat("Working directory: ", getwd()) # Must be set to Project Directory
 
 source("./scripts/common-functions.R")
 source("./scripts/graphing/graph-presets.R")
-source("./analysis/binary-categorical-1/binary-categorical-1-functions.R")
+
 
 # ---- load-packages -----------------------------------------------------------
 library(magrittr)  # pipes
@@ -45,19 +45,18 @@ prints_folder <- paste0("./analysis/nia-lalonde/prints-1/")
 if(!file.exists(prints_folder)){dir.create(file.path(prints_folder))}
 
 # Establish the folder for data cache
-data_cache_folder <- paste0("./data-unshared/derived/nia-lalonde/")
+data_cache_folder <- paste0("./data-private/derived/nia-lalonde/")
 if(!file.exists(data_cache_folder)){dir.create(file.path(data_cache_folder))}
-
-
-
 
 # ---- load-data ---------------------------------------------------------------
 library(twang)
 data(lalonde)
 ds0 <- lalonde%>% as_tibble(); rm(lalonde)
+
 # ---- inspect-data ------------------------------------------------------------
 
 ds0 %>% explore::describe_all()
+
 # ---- tweak-data-1 --------------------------------------------------------------
 
 ds1 <-
@@ -74,9 +73,13 @@ ds1 <-
       ,age > 35 & age <= 45 ~ "36-45"
       ,age > 45 & age <= 55 ~ "46-55"
     )
+    ,re78_cat = case_when(
+      re78 >= 10000 ~ "high"
+      ,re78 < 10000 ~ "low"
+    )
   ) %>%
   mutate_at(
-    .vars = c("educ_cat","age_cat")
+    .vars = c("educ_cat","age_cat", "re78_cat")
     ,.funs = as_factor
   )
 ds1 %>% group_by(educ, educ_cat) %>% count() %>% print(n=nrow(.))
@@ -90,6 +93,7 @@ explore::describe_all(ds1)
 # Histogram examining conversion to categories
 ds1 %>% ggplot(aes(x=age,fill=age_cat))+geom_histogram(binwidth = 1)
 ds1 %>% ggplot(aes(x=educ,fill=educ_cat))+geom_histogram(binwidth = 1)
+ds1 %>% ggplot(aes(x=re78,fill=re78_cat))+geom_histogram(binwidth = 500)
 
 
 # ----- game-plan --------------------------------------------------------------
@@ -119,147 +123,139 @@ ds1 %>% ggplot(aes(x=educ,fill=educ_cat))+geom_histogram(binwidth = 1)
 dependent <- "treat"
 explanatory <- c("age","educ","black","hispan","nodegree","married","re74","re75")
 eq_formula <- as.formula(paste0(dependent," ~ ", paste(explanatory, collapse = " + ") ) )
+eq_formula
 
-d_source <- ds1
-ps1 <-
-  ps(
-    # propensity score model
-    formula            = eq_formula
-    ,estimand          = "ATT" # use ATT because that is consistent with ex-post net impact
-    ,data              = d_source
-    ,sampw             = d_source$survey_weight # !!! optional
-    # gradient boosting
-    ,stop.method       = c("es.mean", "es.max", "ks.mean", "ks.max")
-    ,n.trees           = 5000
-    ,interaction.depth = 2
-    ,shrinkage         = 0.01
-    ,n.minobsinnode    = 10
-    # computational efficiency
-    ,n.keep            = 1
-    ,n.grid            = 25
-    ,ks.exact          = NULL
-    ,version           = "gmb" # gmb, xboost, legacy
-    # other
-    ,verbose           = FALSE
-  )
-rm(d_source)
-# cache for faster reproduction
+# comment out BELOW on reproduction
+# d_source <- ds1 %>% as.data.frame() # to emphasize the source of data
+# ps1 <-
+#   ps(
+#     # propensity score model
+#     formula            = eq_formula
+#     ,estimand          = "ATT" # use ATT because that is consistent with ex-post net impact
+#     ,data              = d_source
+#     ,sampw             = d_source$survey_weight # !!! optional
+#     # gradient boosting
+#     ,stop.method       = c("es.mean", "es.max", "ks.mean", "ks.max")
+#     ,n.trees           = 5000
+#     ,interaction.depth = 2
+#     ,shrinkage         = 0.01
+#     ,n.minobsinnode    = 10
+#     # computational efficiency
+#     ,n.keep            = 1
+#     ,n.grid            = 25
+#     ,ks.exact          = NULL
+#     ,version           = "gmb" # gmb, xboost, legacy
+#     # other
+#     ,verbose           = FALSE
+#   )
+# rm(d_source)
+# ps1 %>%  readr::write_rds(paste0(data_cache_folder,"ps1.rds")) # first time
+# comment out ABOVE on reproduction
 
-# ---- single-model ----------------
+ps1 <-    readr::read_rds(paste0(data_cache_folder,"ps1.rds")) # on reproduction
 
-dependent <- "treat"
+# ----- explore-ps -------------------------------------------------------------
+summary(ps1)
 
-explanatory   <- c("treat", "black","hispan","married", "educ_cat","age","re74", "re75")
-explanatory_r <-c(          "black","hispan","married", "educ_cat","age","re74", "re75")
-#
-# # # finalfit::finalfit(ds2,dependent, explanatory)
-ls_model <- run_logistic_binary(ds1,dependent, explanatory)
-ls_model_r <- run_logistic_binary(ds1,dependent, explanatory_r)
-model <- ls_model$model
-model_reduced <- ls_model_r$model
-model %>% summary()
-model %>% get_rsquared();model_reduced %>% get_rsquared()
-model %>% get_model_fit()
-model %>% broom::tidy(exponentiate =T)
-model %>% broom::glance()
-# model %>% jtools::summ()
-(model %>% sjPlot::plot_model() +
-  labs(title = "Odds Ratios" )+
-    # scale_y_log10( limits = c(.1,3)))%>%
-    scale_y_log10( ))%>%
-  quick_save("full-model-estimates",width=6, height=4)
-model %>% jtools::plot_summs()
-model %>% jtools::plot_coefs()
-model %>% gtsummary::tbl_regression(exp=T) %>% print()
+# ------ estimate-effect -------------------------------------------------------
+compute_effect <- function(
+  ps_object # object from ps() model
+  ,outcome_name = "re78_cat"
+  ,treatment_name = "treat"
+  ,outcome_dist_family = "gaussian" # gaussian for continuous, binomial for discreate outcomes
+){
+  ps_object <- ps1
+  treatment_name = "treat"
 
-#
-# jtools::summ(model)
-# jtools::plot_summs(model)
-# jtools::plot_summs(model,model_reduced, scale=TRUE, plot.distributions=T)
-# ---- testing-models -------
-# source("./analysis/binary-categorical-1/binary-categorical-1-functions.R")
-# dto <- ds2 %>%
-#   get_bi_test_objects(
-#     dependent    = "has_ea"
-#     ,explanatory = c("age_group","sex")
-# ,clear_cache = FALSE # delete if exists
-# ,get_cache = TRUE # TRUE reads the cached object, FALSE computes it
-# )
-# dto %>% make_bi_test_output_A()
-# dto %>% make_bi_test_output_B()
-# table_plot_path <- paste0(prints_folder,dto$model_name,".png" )
-# tt %>%    # build gtsummary table
-#   as_gt() %>%             # convert to gt table
-#   gt::gtsave(             # save table as image
-#     filename = table_plot_path
-# )
+  outcome_name = "re78"
+  outcome_dist_family = "gaussian"
+  # outcome_name = "re78_cat"
+  # outcome_dist_family = "binomial"
 
+  d <-
+    ps_object$data %>%
+    mutate(
+      w =  get.weights(ps_object,stop.method = "es.mean", estimand = "ATT" )
+    ) %>%
+    as_tibble()
+  design.ps <- survey::svydesign(ids= ~1, weights = ~w, data = d)
+  # d %>% group_by(!!rlang::sym(treatment_name)) %>% count()
+  # d %>% group_by(!!!rlang::syms(treatment_name)) %>% count() # for many
 
-
-# ----- compute-models --------------------
-# Generate all graphs to be inserted during reporting
-dependent <- "has_ea"
-explanatory   <- c("employment_state","age_group", "sex","marital_status","hh_role")
-for(i in seq_along(explanatory)){
-  # i <- 1
-  explanatory_i <- c(explanatory[i],explanatory[-i])
-
-  dto <- ds2 %>%
-    get_bi_test_objects(
-      dependent    = dependent
-      ,explanatory = explanatory_i
-      ,clear_cache = TRUE # delete if exists
-      ,get_cache = FALSE # TRUE reads the cached object, FALSE computes it
+  outcome_n <- d %>%
+    group_by(!!rlang::sym(treatment_name)) %>%
+    summarize(
+      outcome_n = sum(!is.na(!!rlang::sym(outcome_name)))
     )
 
-}
-# ---- print-results ------------------
-
-dependent <- "has_ea"
-explanatory   <- c("employment_state","age_group", "sex","marital_status","hh_role")
-for(i in seq_along(explanatory)){
-# for(i in 1 ){
-  # i <- 2
-  explanatory_i <- c(explanatory[i],explanatory[-i])
-
-  dto <- ds2 %>%
-    get_bi_test_objects(
-      dependent    = dependent
-      ,explanatory = explanatory_i
-      ,clear_cache = FALSE # delete if exists
-      ,get_cache = FALSE # TRUE reads the cached object, FALSE computes it
+  ( model_equation <- as.formula(paste0(outcome_name, "~", treatment_name)) )
+  model <-
+    survey::svyglm(model_equation, design = design.ps, family = outcome_dist_family) %>%
+    suppressWarnings()
+  # summary(model)
+  pattern_starts_with_explanatory <- paste0("^",treatment_name, collapse = "|")
+  d_estimates  <-
+    model %>%
+    broom::tidy(
+      conf.int = TRUE
+      # ,exp     = TRUE # converts log-odds into odds-ratios (i.e. =exp(estimate))
+    ) %>%
+    mutate(
+      # conv_odds    = (estimate-1) # careful, this relies on broom::tidy(exp=TRUE)
+      # otherwise: exp(estimate) - 1
+       var_name    = stringr::str_extract(term, pattern_starts_with_explanatory)
+      ,value_level = stringr::str_remove( term, pattern_starts_with_explanatory)
     )
-  dto %>% make_bi_test_output_A(d=ds2)
-  # dto %>% make_bi_test_output_B()
-  rm(dto)
+  d_estimates
+
+  # applies propensity weights
+  d_net <-
+    d %>%
+    group_by(treat) %>%
+    summarize(
+      unweighted = mean(treat, na.rm=T)
+      ,weighted = sum(treat*w, na.rm =T) / sum(w, na.rm = T)
+    ) %>%
+    ungroup() %>%
+    mutate(
+      unw_diff = unweighted - lag(unweighted)
+      ,wgt_diff = weighted - lag(weighted)
+    )
+
+  d_result <-
+    dplyr::left_join(
+      d_net %>%
+        select(
+          !!rlang::sym(treatment_name)
+          ,mean_raw      = unweighted
+          ,mean_weighted = weighted
+          ,net_impact    = wgt_diff
+        )
+      ,
+      d_estimates %>%
+        filter(term==paste0(treatment_name,"TRUE")) %>%
+        # mutate(tx=TRUE) %>%
+        mutate(!!rlang::sym(treatment_name):=TRUE) %>%
+        select(!!rlang::sym(treatment_name),conv_odds,p.value)
+      ,by = treatment_name
+    )
+
+  d_result <-
+    d_result %>%
+    left_join(outcome_n,by = "tx")%>%
+    mutate(
+      outcome = outcome_name
+    ) %>%
+    select(outcome,outcome_n,tx, everything())
+  return(d_result)
 }
+# How to use
+# ps1 %>% compute_effect("emp_at_survey")
+# ps1 %>% compute_effect("employed_postprogram")
+# ps1 %>% compute_effect("social_assistance")
 
-# ---- paste-results ------------------
-
-dependent <- "has_ea"
-explanatory   <- c("employment_state","age_group", "sex","marital_status","hh_role")
-for(i in seq_along(explanatory)){
-  # i <- 1
-  explanatory_i <- c(explanatory[i],explanatory[-i])
-  model_name <- paste0(c(dependent,explanatory_i),collapse = "-")
-  display_a <- paste0(prints_folder,model_name,".jpg")
-  display_b <- paste0(prints_folder,model_name,".png")
- cat("\n##",i,"\n")
-  cat("\n")
- display_a %>% jpeg::readJPEG() %>% grid::grid.raster()
- cat("\n")
- # display_b %>% png::readPNG() %>% grid::grid.raster()
- # display_b %>% jpeg::readJPEG() %>% grid::grid.raster()
- cat("\n")
- }
-
-# ---- graph-1 -----------------------------------------------------------------
-
-
-# ---- graph-2 -----------------------------------------------------------------
-
-# ---- save-to-disk ------------------------------------------------------------
-path <- "./analysis/binary-categorical-1/binary-categorical-1.Rmd"
+# ---- publish -----------------------------------------------------------------
+path <- "./analysis/nia-lalonde/nia-lalonde.Rmd"
 rmarkdown::render(
   input = path ,
   output_format=c(
