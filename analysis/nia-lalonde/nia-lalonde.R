6ef8f7d71d68abde1b6aff83078f108a5f8d2782
Andriy V. Koval <koval.andrey@gmail.com>
2021-12-20 08:46:57 -0700

    rename

---
+++ analysis/nia-lalonde/nia-lalonde.R
@@ -0,0 +1,271 @@
+rm(list = ls(all.names = TRUE)) # Clear the memory of variables from previous run. This is not called by knitr, because it's above the first chunk.
+cat("\014") # Clear the console
+
+# verify root location
+cat("Working directory: ", getwd()) # Must be set to Project Directory
+# if the line above DOES NOT generates the project root, re-map by selecting
+# Session --> Set Working Directory --> To Project Directory location
+# Project Directory should be the root by default unless overwritten
+
+# ---- load-sources ------------------------------------------------------------
+
+source("./scripts/common-functions.R")
+source("./scripts/graphing/graph-presets.R")
+source("./analysis/binary-categorical-1/binary-categorical-1-functions.R")
+
+# ---- load-packages -----------------------------------------------------------
+library(magrittr)  # pipes
+library(dplyr)     # data wrangling
+library(ggplot2)   # graphs
+library(janitor)   # tidy data
+library(tidyr)     # data wrangling
+library(forcats)   # factors
+library(stringr)   # strings
+library(lubridate) # dates
+library(readxl)    # for import
+library(explore)   # for describe_all()
+library(scales)    # formatting
+library(rlang)     # tidy evals - see https://edwinth.github.io/blog/dplyr-recipes/ and
+library(labelled)  # labels - see https://cran.r-project.org/web/packages/labelled/vignettes/intro_labelled.html
+library(gtsummary) # tables for regression
+library(gt)        # tables
+library(pROC)      # ROC curve calculation, `ggroc()`
+
+# check out these packages for model tables
+# sjPlot
+# jtools
+
+# ---- declare-functions -------------------------------------------------------
+# local to this script
+
+
+# ---- declare-globals ---------------------------------------------------------
+# Establish the folder for printing plots
+prints_folder <- paste0("./analysis/nia-lalonde/prints-1/")
+if(!file.exists(prints_folder)){dir.create(file.path(prints_folder))}
+
+# Establish the folder for data cache
+data_cache_folder <- paste0("./data-unshared/derived/nia-lalonde/")
+if(!file.exists(data_cache_folder)){dir.create(file.path(data_cache_folder))}
+
+
+
+
+# ---- load-data ---------------------------------------------------------------
+library(twang)
+data(lalonde)
+ds0 <- lalonde%>% as_tibble(); rm(lalonde)
+# ---- inspect-data ------------------------------------------------------------
+
+ds0 %>% explore::describe_all()
+# ---- tweak-data-1 --------------------------------------------------------------
+
+ds1 <-
+  ds0 %>%
+  mutate(
+    educ_cat = case_when(
+      educ < 12 ~ "less than HS"
+      ,educ == 12 ~ "HS"
+      ,educ > 12 ~ "more than HS"
+    )
+    ,age_cat = case_when(
+       age > 15 & age <= 25 ~ "16-25"
+      ,age > 25 & age <= 35 ~ "26-35"
+      ,age > 35 & age <= 45 ~ "36-45"
+      ,age > 45 & age <= 55 ~ "46-55"
+    )
+  ) %>%
+  mutate_at(
+    .vars = c("educ_cat","age_cat")
+    ,.funs = as_factor
+  )
+ds1 %>% group_by(educ, educ_cat) %>% count() %>% print(n=nrow(.))
+ds1 %>% group_by(age, age_cat) %>% count() %>% print(n=nrow(.))
+
+# ---- explore-data ------------------------------------------------------------
+glimpse(ds1)
+look_for(ds1)
+explore::describe_all(ds1)
+
+# Histogram examining conversion to categories
+ds1 %>% ggplot(aes(x=age,fill=age_cat))+geom_histogram(binwidth = 1)
+ds1 %>% ggplot(aes(x=educ,fill=educ_cat))+geom_histogram(binwidth = 1)
+
+
+# ----- game-plan --------------------------------------------------------------
+# Steps in the PSA analysis described in [twang() vignette](https://cran.r-project.org/web/packages/twang/vignettes/twang.pdf)
+# 1. Determine the estimand (ATT)
+# 2. Determine the observed confounding factors to be balanced (covariate list)
+# 3. Fit the propensity score model
+# 3(a) Evaluate the convergence of the algorithm
+# 3(b) Assess the balance of confounding factors before and after applying the propensity score weights
+# 3(c) Rerun if needed tweaking algorithm features
+# 4. Estimate the net effect
+
+# Estimands
+
+# ATT - Average Treatment Effect on the Treated
+# a = what actually happened to the TREATED group (i.e. mean or median)
+# b = what would have happened if TREATED group did NOT receive Tx (i.e. mean or median)
+# ATT = a - b
+
+# ATE - Average Treatment Effect (on everyone) =
+# a =  what would have happend had EVERYONE received Tx
+# b =  what would have happened had NOONE received Tx
+# ATE = a - b
+
+
+# ---- estimate-ps -------------------------------------------------------------
+dependent <- "treat"
+explanatory <- c("age","educ","black","hispan","nodegree","married","re74","re75")
+eq_formula <- as.formula(paste0(dependent," ~ ", paste(explanatory, collapse = " + ") ) )
+
+d_source <- ds1
+ps1 <-
+  ps(
+    # propensity score model
+    formula            = eq_formula
+    ,estimand          = "ATT" # use ATT because that is consistent with ex-post net impact
+    ,data              = d_source
+    ,sampw             = d_source$survey_weight # !!! optional
+    # gradient boosting
+    ,stop.method       = c("es.mean", "es.max", "ks.mean", "ks.max")
+    ,n.trees           = 5000
+    ,interaction.depth = 2
+    ,shrinkage         = 0.01
+    ,n.minobsinnode    = 10
+    # computational efficiency
+    ,n.keep            = 1
+    ,n.grid            = 25
+    ,ks.exact          = NULL
+    ,version           = "gmb" # gmb, xboost, legacy
+    # other
+    ,verbose           = FALSE
+  )
+rm(d_source)
+# cache for faster reproduction
+
+# ---- single-model ----------------
+
+dependent <- "treat"
+
+explanatory   <- c("treat", "black","hispan","married", "educ_cat","age","re74", "re75")
+explanatory_r <-c(          "black","hispan","married", "educ_cat","age","re74", "re75")
+#
+# # # finalfit::finalfit(ds2,dependent, explanatory)
+ls_model <- run_logistic_binary(ds1,dependent, explanatory)
+ls_model_r <- run_logistic_binary(ds1,dependent, explanatory_r)
+model <- ls_model$model
+model_reduced <- ls_model_r$model
+model %>% summary()
+model %>% get_rsquared();model_reduced %>% get_rsquared()
+model %>% get_model_fit()
+model %>% broom::tidy(exponentiate =T)
+model %>% broom::glance()
+# model %>% jtools::summ()
+(model %>% sjPlot::plot_model() +
+  labs(title = "Odds Ratios" )+
+    # scale_y_log10( limits = c(.1,3)))%>%
+    scale_y_log10( ))%>%
+  quick_save("full-model-estimates",width=6, height=4)
+model %>% jtools::plot_summs()
+model %>% jtools::plot_coefs()
+model %>% gtsummary::tbl_regression(exp=T) %>% print()
+
+#
+# jtools::summ(model)
+# jtools::plot_summs(model)
+# jtools::plot_summs(model,model_reduced, scale=TRUE, plot.distributions=T)
+# ---- testing-models -------
+# source("./analysis/binary-categorical-1/binary-categorical-1-functions.R")
+# dto <- ds2 %>%
+#   get_bi_test_objects(
+#     dependent    = "has_ea"
+#     ,explanatory = c("age_group","sex")
+# ,clear_cache = FALSE # delete if exists
+# ,get_cache = TRUE # TRUE reads the cached object, FALSE computes it
+# )
+# dto %>% make_bi_test_output_A()
+# dto %>% make_bi_test_output_B()
+# table_plot_path <- paste0(prints_folder,dto$model_name,".png" )
+# tt %>%    # build gtsummary table
+#   as_gt() %>%             # convert to gt table
+#   gt::gtsave(             # save table as image
+#     filename = table_plot_path
+# )
+
+
+
+# ----- compute-models --------------------
+# Generate all graphs to be inserted during reporting
+dependent <- "has_ea"
+explanatory   <- c("employment_state","age_group", "sex","marital_status","hh_role")
+for(i in seq_along(explanatory)){
+  # i <- 1
+  explanatory_i <- c(explanatory[i],explanatory[-i])
+
+  dto <- ds2 %>%
+    get_bi_test_objects(
+      dependent    = dependent
+      ,explanatory = explanatory_i
+      ,clear_cache = TRUE # delete if exists
+      ,get_cache = FALSE # TRUE reads the cached object, FALSE computes it
+    )
+
+}
+# ---- print-results ------------------
+
+dependent <- "has_ea"
+explanatory   <- c("employment_state","age_group", "sex","marital_status","hh_role")
+for(i in seq_along(explanatory)){
+# for(i in 1 ){
+  # i <- 2
+  explanatory_i <- c(explanatory[i],explanatory[-i])
+
+  dto <- ds2 %>%
+    get_bi_test_objects(
+      dependent    = dependent
+      ,explanatory = explanatory_i
+      ,clear_cache = FALSE # delete if exists
+      ,get_cache = FALSE # TRUE reads the cached object, FALSE computes it
+    )
+  dto %>% make_bi_test_output_A(d=ds2)
+  # dto %>% make_bi_test_output_B()
+  rm(dto)
+}
+
+# ---- paste-results ------------------
+
+dependent <- "has_ea"
+explanatory   <- c("employment_state","age_group", "sex","marital_status","hh_role")
+for(i in seq_along(explanatory)){
+  # i <- 1
+  explanatory_i <- c(explanatory[i],explanatory[-i])
+  model_name <- paste0(c(dependent,explanatory_i),collapse = "-")
+  display_a <- paste0(prints_folder,model_name,".jpg")
+  display_b <- paste0(prints_folder,model_name,".png")
+ cat("\n##",i,"\n")
+  cat("\n")
+ display_a %>% jpeg::readJPEG() %>% grid::grid.raster()
+ cat("\n")
+ # display_b %>% png::readPNG() %>% grid::grid.raster()
+ # display_b %>% jpeg::readJPEG() %>% grid::grid.raster()
+ cat("\n")
+ }
+
+# ---- graph-1 -----------------------------------------------------------------
+
+
+# ---- graph-2 -----------------------------------------------------------------
+
+# ---- save-to-disk ------------------------------------------------------------
+path <- "./analysis/binary-categorical-1/binary-categorical-1.Rmd"
+rmarkdown::render(
+  input = path ,
+  output_format=c(
+    "html_document"
+    # "word_document"
+    # "pdf_document"
+  ),
+  clean=TRUE
+)
